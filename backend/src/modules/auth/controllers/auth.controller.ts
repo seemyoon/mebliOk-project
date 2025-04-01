44690e1ebdf616398f0bc3493508109f1e551d77
Alexandr Semenets <alexandrsemenec01@gmail.com>
2025-04-01 15:40:14 +0300

    [full auth 2 part], [mail.module.ts]

--- backend/src/modules/auth/controllers/auth.controller.ts
+++ backend/src/modules/auth/controllers/auth.controller.ts
@@ -70,7 +70,7 @@ export class AuthController {
     @CurrentUser() userData: IUserData,
     @Body() dto: ForgotPasswordReqDto,
   ): Promise<void> {
-    await this.authService.forgotPasswordSendmail(userData, dto);
+    await this.authService.forgotPasswordSendфывmail(userData, dto);
   }
 
   @SkipAuth()
